8ff16a5a64452f85ffbf9870c588231106a1acc6
Stream <1542763342@qq.com>
2025-05-10 02:59:45 +0800

    build: project structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,20 @@
-rootProject.name = "GenKt-root"
-
-val projects = listOf(
-    "core"
-)
-
-include(projects)
-
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositories {
         mavenCentral()
     }
 }
+
+rootProject.name = "GenKt"
+
+fun genKtModules(vararg modules: String) {
+    for (module in modules) {
+        include(":genkt-$module")
+        project(":genkt-$module").projectDir = file(module)
+    }
+}
+
+genKtModules(
+    "core"
+)
+
