fe83af1c25efd6f3cb834fcd8146b52971bded97
Stream <1542763342@qq.com>
2025-05-11 22:28:11 +0800

    feat(jsonrpc): basic transport

--- jsonrpc-core/src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/Transport.kt
+++ jsonrpc-core/src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/Transport.kt
@@ -2,11 +2,18 @@ package io.modelcontextprotocol.kotlin.sdk
 
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.SendChannel
+import kotlinx.io.Sink
+import kotlinx.io.Source
 import kotlinx.serialization.json.JsonObject
 
 public data class JsonTransport(
     public val sendChannel: SendChannel<JsonObject>,
     public val receiveChannel: ReceiveChannel<JsonObject>
+)
+
+public data class ByteTransport(
+    public val source: Source,
+    public val sink: Sink
 )
 
 public data class JsonRpcClientTransport(
