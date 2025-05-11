6d5cc165d8f4d85e2c73e0a14104c5634669d80d
Stream <1542763342@qq.com>
2025-05-11 23:11:33 +0800

    feat(jsonrpc-transport-stdio): migrate StdioTransport StringTransport

--- jsonrpc-transport-stdio/src/commonMain/kotlin/io/github/genkt/jsonrpc/transport/stdio/StdioTransport.kt
+++ jsonrpc-transport-stdio/src/commonMain/kotlin/io/github/genkt/jsonrpc/transport/stdio/StdioTransport.kt
@@ -1,34 +1,28 @@
 package io.github.genkt.jsonrpc.transport.stdio
 
-import io.modelcontextprotocol.kotlin.sdk.ByteTransport
+import io.modelcontextprotocol.kotlin.sdk.StringTransport
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.io.Buffer
-import kotlinx.io.readLine
-import kotlinx.io.writeString
 
 @Suppress("FunctionName")
 @OptIn(DelicateCoroutinesApi::class)
-public fun StdioTransport(): ByteTransport {
-    val stdin = Channel<String>()
+public fun StdioTransport(): StringTransport {
+    val input = Channel<String>()
     GlobalScope.launch {
         while (currentCoroutineContext().isActive) {
             val line = readlnOrNull() ?: break
-            stdin.send(line)
+            input.send(line)
         }
     }
-    val inputBuffer = Buffer()
-    GlobalScope.launch { stdin.consumeEach { inputBuffer.writeString(it) } }
-    val outputBuffer = Buffer()
+    val output = Channel<String>()
     GlobalScope.launch {
         while (currentCoroutineContext().isActive) {
-            val line = outputBuffer.readLine() ?: break
-            println(line)
+            output.consumeEach { print(it) }
         }
     }
-    return ByteTransport(
-        source = inputBuffer,
-        sink = outputBuffer,
+    return StringTransport(
+        sendChannel = output,
+        receiveChannel = input,
     )
 }
