72373a58d2a672934f9dcdd0bfed397c2cb04f11
Stream <1542763342@qq.com>
2025-05-13 01:44:04 +0800

    feat(serialization-json): implement logic

---
+++ serialization-json/src/commonMain/kotlin/io/github/genkt/serialization/json/JsonPolymorphicSerializer.kt
@@ -0,0 +1,28 @@
+package io.github.genkt.serialization.json
+
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonElement
+
+public class JsonPolymorphicSerializer<T>(
+    public override val descriptor: SerialDescriptor,
+    public val selectSerializer: (T) -> SerializationStrategy<T>,
+    public val selectDeserializer: (JsonElement) -> DeserializationStrategy<T>,
+) : KSerializer<T> {
+    override fun serialize(encoder: Encoder, value: T) {
+        selectSerializer(value).serialize(encoder, value)
+    }
+
+    override fun deserialize(decoder: Decoder): T {
+        val jsonDecoder = decoder as? JsonDecoder
+            ?: throw IllegalArgumentException("This serializer can be used only with Json format")
+        val element = jsonDecoder.decodeJsonElement()
+        val deserializer = selectDeserializer(element)
+        return jsonDecoder.json.decodeFromJsonElement(deserializer, element)
+    }
+}
