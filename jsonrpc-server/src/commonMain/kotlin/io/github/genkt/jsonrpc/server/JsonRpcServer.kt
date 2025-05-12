0e6494006b2c2676a05897754c206c3e979711c0
Stream <1542763342@qq.com>
2025-05-13 04:39:33 +0800

    refactor(jsonrpc-server): enhance error handling and improve coroutine safety in JsonRpcServer

--- jsonrpc-server/src/commonMain/kotlin/io/github/genkt/jsonrpc/server/JsonRpcServer.kt
+++ jsonrpc-server/src/commonMain/kotlin/io/github/genkt/jsonrpc/server/JsonRpcServer.kt
@@ -2,55 +2,87 @@ package io.github.genkt.jsonrpc.server
 
 import io.github.genkt.jsonrpc.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.cancellable
 import kotlinx.serialization.json.JsonPrimitive
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.cancellation.CancellationException
+import kotlin.coroutines.coroutineContext
+
+public fun CoroutineScope.JsonRpcServer(
+    transport: JsonRpcServerTransport,
+    onRequest: suspend CoroutineScope.(JsonRpcRequest) -> JsonRpcServerMessage,
+    onNotification: suspend CoroutineScope.(JsonRpcNotification) -> Unit,
+    errorHandler: suspend (Throwable) -> Unit = defaultErrorHandler
+): JsonRpcServer = JsonRpcServer(
+    transport,
+    onRequest,
+    onNotification,
+    coroutineContext,
+    errorHandler
+)
 
 public class JsonRpcServer(
     public val transport: JsonRpcServerTransport,
-    onRequest: suspend CoroutineScope.(JsonRpcRequest) -> JsonRpcServerMessage,
+    private val onRequest: suspend CoroutineScope.(JsonRpcRequest) -> JsonRpcServerMessage,
     private val onNotification: suspend CoroutineScope.(JsonRpcNotification) -> Unit,
     coroutineContext: CoroutineContext = Dispatchers.Default + Job(),
+    private val errorHandler: suspend (Throwable) -> Unit = defaultErrorHandler
 ) : AutoCloseable {
-    private val onRequest = onRequest.safeHandler()
     private val coroutineScope = CoroutineScope(coroutineContext)
     private val receiveJob = coroutineScope.launch {
-        transport.receiveFlow
+        transport.receiveFlow.cancellable()
             .collect { handleMessageSafe(it) }
     }
 
     private fun CoroutineScope.handleMessageSafe(request: JsonRpcClientMessage) {
         launch(receiveJob) {
             when (request) {
-                is JsonRpcRequest -> transport.sendChannel.send(onRequest(request))
-                is JsonRpcNotification -> onNotification(request)
+                is JsonRpcRequest -> onRequestSafe(request)
+                is JsonRpcNotification -> onNotificationSafe(request)
                 is JsonRpcClientMessageBatch -> request.messages.forEach { handleMessageSafe(it) }
             }
         }
     }
 
-    override fun close() {
-        receiveJob.cancel()
-        transport.close()
-    }
-}
-
-private fun (suspend CoroutineScope.(JsonRpcRequest) -> JsonRpcServerMessage).safeHandler():
-        (suspend CoroutineScope.(JsonRpcRequest) -> JsonRpcServerMessage) {
-    return { request ->
+    private suspend fun CoroutineScope.onNotificationSafe(request: JsonRpcNotification) {
         try {
-            this.this@safeHandler(request)
+            onNotification(request)
         } catch (e: CancellationException) {
             throw e
         } catch (e: Throwable) {
+            errorHandler(e)
+        }
+    }
+
+    private suspend fun CoroutineScope.onRequestSafe(request: JsonRpcRequest) {
+        val response = try {
+            onRequest(request)
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Throwable) {
+            errorHandler(e)
             JsonRpcFailResponse(
-                id = request.id,
-                error = JsonRpcFailResponse.Error(
+                request.id,
+                JsonRpcFailResponse.Error(
                     code = JsonRpcFailResponse.Error.Code.InternalError,
                     message = e.message ?: "Internal error",
                     data = JsonPrimitive(e.stackTraceToString())
                 )
             )
         }
+        transport.sendChannel.send(response)
+    }
+
+    override fun close() {
+        coroutineScope.cancel()
+        transport.close()
+    }
+}
+
+private val defaultErrorHandler: suspend (Throwable) -> Unit = { throwable ->
+    if (throwable is CancellationException) {
+        throw throwable
+    } else {
+        coroutineContext[CoroutineExceptionHandler]?.handleException(coroutineContext, throwable)
     }
 }
