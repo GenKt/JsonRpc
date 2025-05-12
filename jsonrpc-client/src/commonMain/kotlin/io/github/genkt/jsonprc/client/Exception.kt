61ad0c2235c57869f0cca7f173ba7addd7679049
Stream <1542763342@qq.com>
2025-05-13 07:16:00 +0800

    refactor(jsonrpc-client): change Exception content

--- jsonrpc-client/src/commonMain/kotlin/io/github/genkt/jsonprc/client/Exception.kt
+++ jsonrpc-client/src/commonMain/kotlin/io/github/genkt/jsonprc/client/Exception.kt
@@ -2,17 +2,17 @@ package io.github.genkt.jsonprc.client
 
 import io.github.genkt.jsonrpc.JsonRpcFailResponse
 import io.github.genkt.jsonrpc.JsonRpcRequest
-import io.github.genkt.jsonrpc.RequestId
+import io.github.genkt.jsonrpc.JsonRpcServerSingleMessage
 import kotlin.time.Duration
 
-public class JsonRpcResponseException(public val error: JsonRpcFailResponse.Error):
+public class JsonRpcResponseException(public val error: JsonRpcFailResponse.Error) :
     RuntimeException("Server response with error:(${error.code}) ${error.message}")
 
 public class JsonRpcTimeoutException(
     public val originalRequest: JsonRpcRequest,
     public val timeout: Duration,
-): RuntimeException("Server response timeout exceeded: ($timeout), with request: $originalRequest")
+) : RuntimeException("Server response timeout exceeded: ($timeout), with request: $originalRequest")
 
 public class JsonRpcRequestIdNotFoundException(
-    public val requestId: RequestId,
-): RuntimeException("Request id not found: $requestId")
+    public val response: JsonRpcServerSingleMessage,
+) : RuntimeException("Request id not found: $response")
