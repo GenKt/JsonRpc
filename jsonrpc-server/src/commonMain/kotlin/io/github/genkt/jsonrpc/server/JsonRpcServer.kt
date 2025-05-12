8bed3b0af57f93d610e3ac9365504e89039c07ea
Stream <1542763342@qq.com>
2025-05-12 22:49:07 +0800

    fix(jsonrpc-server): processing request won't block `receiveJob` and exception will propagate correctly

--- jsonrpc-server/src/commonMain/kotlin/io/github/genkt/jsonrpc/server/JsonRpcServer.kt
+++ jsonrpc-server/src/commonMain/kotlin/io/github/genkt/jsonrpc/server/JsonRpcServer.kt
@@ -1,30 +1,30 @@
 package io.github.genkt.jsonrpc.server
 
 import io.github.genkt.jsonrpc.*
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 public class JsonRpcServer(
     public val transport: JsonRpcServerTransport,
     public val onRequest: suspend (JsonRpcRequest) -> JsonRpcServerMessage,
-    public val onNotification: suspend (JsonRpcNotification) -> Unit,
-    public val coroutineContext: CoroutineContext = Dispatchers.Default,
+    public val onNotification: suspend CoroutineScope.(JsonRpcNotification) -> Unit,
+    public val coroutineContext: CoroutineContext = Dispatchers.Default + SupervisorJob(),
 ) : AutoCloseable {
-    private val receiveJob = CoroutineScope(coroutineContext).launch {
+    private val coroutineScope = CoroutineScope(coroutineContext)
+    private val receiveJob = coroutineScope.launch {
         transport.receiveFlow
             .catch { coroutineContext.handleException(it) }
             .collect { handleMessage(it) }
     }
 
-    private suspend fun handleMessage(request: JsonRpcClientMessage) {
-        when (request) {
-            is JsonRpcRequest -> transport.sendChannel.send(onRequest(request))
-            is JsonRpcNotification -> onNotification(request)
-            is JsonRpcClientMessageBatch -> request.messages.forEach { handleMessage(it) }
+    private fun CoroutineScope.handleMessage(request: JsonRpcClientMessage) {
+        launch(receiveJob) {
+            when (request) {
+                is JsonRpcRequest -> transport.sendChannel.send(onRequest(request))
+                is JsonRpcNotification -> onNotification(request)
+                is JsonRpcClientMessageBatch -> request.messages.forEach { handleMessage(it) }
+            }
         }
     }
 
