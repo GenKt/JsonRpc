192da3a3dcff345cff693a44d5a3407056c1889d
ConstasJ <constasj@qq.com>
2025-05-14 20:56:43 +0800

    chore(logging): introduce kotlinx.datetime and kotlinx.io

--- logging-api/build.gradle.kts
+++ logging-api/build.gradle.kts
@@ -2,3 +2,13 @@ plugins {
     id("kotlin-multiplatform-full")
     id("publishing-convention")
 }
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(libs.kotlinx.datetime)
+            }
+        }
+    }
+}
