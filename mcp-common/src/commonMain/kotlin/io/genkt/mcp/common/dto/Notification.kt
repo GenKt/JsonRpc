fc4b93705bcbda46f402cfd6a166a67e4bcdfd3b
ConstasJ <constasj@qq.com>
2025-05-15 10:41:45 +0800

    feat(mcp-common): add McpNotification to represent notification objects

---
+++ mcp-common/src/commonMain/kotlin/io/genkt/mcp/common/dto/Notification.kt
@@ -0,0 +1,26 @@
+package io.genkt.mcp.common.dto
+
+import kotlinx.serialization.Serializable
+
+public enum class ListChangeResource {
+    ROOTS,
+    PROMPTS,
+    RESOURCES,
+    TOOLS,
+}
+
+public sealed interface McpNotification {
+    public data class ListChange(
+        public val type: ListChangeResource,
+    ): McpNotification
+
+    @Serializable
+    public data class SubscribeResource(
+        public val uri: String,
+    ): McpNotification
+
+    @Serializable
+    public data class LogMessage(
+        public val message: McpLogging.LogMessage,
+    ): McpNotification
+}
