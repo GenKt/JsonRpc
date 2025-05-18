1aacf1102e1d307512e0ea7e60595586203620ee
Stream <1542763342@qq.com>
2025-05-18 19:45:55 +0800

    feat(dto): add companion objects to McpCall-related interfaces

--- mcp-common/src/commonMain/kotlin/io/genkt/mcp/common/dto/Call.kt
+++ mcp-common/src/commonMain/kotlin/io/genkt/mcp/common/dto/Call.kt
@@ -7,14 +7,19 @@ import kotlinx.serialization.json.JsonElement
 public sealed interface McpCall<out R> {
     public val method: String
     public val param: JsonElement
+    public companion object
 }
 public sealed interface McpProgressRequest<out Result, out Request>: McpCall<Result> {
     public val token: McpProgress.Token
     public val request: Request
     public val progressChannel: SendChannel<McpProgress.ProgressNotification>
 }
-public sealed interface McpServerCall<out R>: McpCall<R>
-public sealed interface McpClientCall<out R>: McpCall<R>
+public sealed interface McpServerCall<out R>: McpCall<R> {
+    public companion object
+}
+public sealed interface McpClientCall<out R>: McpCall<R> {
+    public companion object
+}
 public sealed interface McpClientRequest<out R>: McpClientCall<R> {
     public val resultDeserializer: DeserializationStrategy<R>
 }
