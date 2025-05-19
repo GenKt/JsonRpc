8ffc03b0bd2d0cd64ad66bfe380acbf9f64d549a
Stream <1542763342@qq.com>
2025-05-19 21:04:31 +0800

    refactor(serialize-json): make meaningful serialDescriptor for JsonPolymorphicSerializer

--- serialization-json/src/commonMain/kotlin/io/genkt/serialization/json/JsonPolymorphicSerializer.kt
+++ serialization-json/src/commonMain/kotlin/io/genkt/serialization/json/JsonPolymorphicSerializer.kt
@@ -1,19 +1,41 @@
 package io.genkt.serialization.json
 
-import kotlinx.serialization.DeserializationStrategy
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.*
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.PolymorphicKind
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.SerialKind
+import kotlinx.serialization.descriptors.buildSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonDecoder
 import kotlinx.serialization.json.JsonElement
 
+@OptIn(ExperimentalSerializationApi::class, InternalSerializationApi::class)
 public class JsonPolymorphicSerializer<T>(
-    public override val descriptor: SerialDescriptor,
+    public val serialName: String,
+    public val childSerializers: List<KSerializer<out T>>,
     public val selectSerializer: (T) -> SerializationStrategy<*>,
     public val selectDeserializer: (JsonElement) -> DeserializationStrategy<T>,
+    public val annotations: List<Annotation> = emptyList(),
 ) : KSerializer<T> {
+    override val descriptor: SerialDescriptor by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        check(childSerializers.asSequence().map { it.descriptor.serialName }.distinct().count() == childSerializers.size) {
+            "Serializers for polymorphic class '$serialName' have duplicate names: ${childSerializers.map { it.descriptor.serialName }}"
+        }
+        buildSerialDescriptor(serialName, PolymorphicKind.SEALED) {
+            element("type", String.serializer().descriptor)
+            val elementDescriptor =
+                buildSerialDescriptor("kotlinx.serialization.Sealed<$serialName>", SerialKind.CONTEXTUAL) {
+                    childSerializers.forEach {
+                        element(it.descriptor.serialName, it.descriptor)
+                    }
+                }
+            element("value", elementDescriptor)
+            annotations = this@JsonPolymorphicSerializer.annotations
+        }
+    }
+
     @Suppress("unchecked_cast")
     override fun serialize(encoder: Encoder, value: T) {
         val serializer = selectSerializer(value) as SerializationStrategy<T>
