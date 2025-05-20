3cf8c01986ab4df3a511096c3f41b4acf9cb0f61
Stream <1542763342@qq.com>
2025-05-20 13:19:15 +0800

    refactor(mcp-common): add @Serializable for Call.kt
    TODO: serializer

--- mcp-common/src/commonMain/kotlin/io/genkt/mcp/common/dto/Call.kt
+++ mcp-common/src/commonMain/kotlin/io/genkt/mcp/common/dto/Call.kt
@@ -1,32 +1,49 @@
 package io.genkt.mcp.common.dto
 
 import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.Serializable
 
 public sealed interface McpCall<out R> {
     public val method: String
+
     public companion object
 }
-public sealed interface McpProgressRequest<out Result, out Request>: McpCall<Result> {
+
+public sealed interface McpProgressRequest<out Result, out Request> : McpCall<Result> {
     public val token: McpProgress.Token
     public val request: Request
 }
-public sealed interface McpServerCall<out R>: McpCall<R> {
+
+@Serializable
+public sealed interface McpServerCall<out R> : McpCall<R> {
     public companion object
 }
-public sealed interface McpClientCall<out R>: McpCall<R> {
+
+@Serializable
+public sealed interface McpClientCall<out R> : McpCall<R> {
     public companion object
 }
-public sealed interface McpClientRequest<out R>: McpClientCall<R> {
+
+@Serializable
+public sealed interface McpClientRequest<out R> : McpClientCall<R> {
     public val resultDeserializer: DeserializationStrategy<R>
+
     public companion object
 }
-public sealed interface McpClientNotification: McpClientCall<Unit> {
+
+@Serializable
+public sealed interface McpClientNotification : McpClientCall<Unit> {
     public companion object
 }
-public sealed interface McpServerRequest<out R>: McpServerCall<R> {
+
+@Serializable
+public sealed interface McpServerRequest<out R> : McpServerCall<R> {
     public val resultDeserializer: DeserializationStrategy<R>
+
     public companion object
 }
-public sealed interface McpServerNotification: McpServerCall<Unit> {
+
+@Serializable
+public sealed interface McpServerNotification : McpServerCall<Unit> {
     public companion object
 }
