78c79b3168c461a9aab5415bf8d8ccba15e7750c
Stream <1542763342@qq.com>
2025-05-25 17:49:54 +0800

    refactor(mcp-client): remove not necessary properties in McpClient

--- mcp-client/src/commonMain/kotlin/io/genkt/mcp/client/Client.kt
+++ mcp-client/src/commonMain/kotlin/io/genkt/mcp/client/Client.kt
@@ -1,26 +1,15 @@
 package io.genkt.mcp.client
 
-import io.genkt.jsonprc.client.JsonRpcClient
 import io.genkt.jsonrpc.JsonRpcTransport
 import io.genkt.jsonrpc.RequestId
-import io.genkt.jsonrpc.server.JsonRpcServer
 import io.genkt.mcp.common.dto.*
 import kotlinx.coroutines.CoroutineScope
 
 public interface McpClient {
     public val info: McpInit.Implementation
     public val capabilities: McpInit.ClientCapabilities
-    public val onRoot: suspend (McpRoot.ListRequest) -> McpRoot.ListResponse
-    public val onSampling: suspend (McpSampling.CreateMessageRequest) -> McpSampling.CreateMessageResult
-    public val onNotification: suspend (McpServerNotification) -> Unit
     public val transport: JsonRpcTransport
-    public val requestIdGenerator: () -> RequestId
-    public val progressTokenGenerator: () -> McpProgress.Token
-    public val errorHandler: suspend CoroutineScope.(Throwable) -> Unit
-
-    public val jsonRpcServer: JsonRpcServer
-
-    public val jsonRpcClient: JsonRpcClient
+    public val uncaughtErrorHandler: suspend CoroutineScope.(Throwable) -> Unit
 
     public val coroutineScope: CoroutineScope
     public suspend fun <R> call(mcpCall: McpClientCall<R>): R
