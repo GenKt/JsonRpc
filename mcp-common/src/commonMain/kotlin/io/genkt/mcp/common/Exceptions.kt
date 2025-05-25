e672cfe4fb61704215ce46ad8d95c06d244fb553
Stream <1542763342@qq.com>
2025-05-26 02:00:33 +0800

    feat(mcp-common): add McpErrorResponseException for handling JsonRpcFailResponse

--- mcp-common/src/commonMain/kotlin/io/genkt/mcp/common/Exceptions.kt
+++ mcp-common/src/commonMain/kotlin/io/genkt/mcp/common/Exceptions.kt
@@ -1,6 +1,7 @@
 package io.genkt.mcp.common
 
 import io.genkt.jsonrpc.JsonRpcClientSingleMessage
+import io.genkt.jsonrpc.JsonRpcFailResponse
 
 public class McpMethodNotFoundException(
     public val originalCall: JsonRpcClientSingleMessage
@@ -10,3 +11,14 @@ public class McpParamParseException(
     public val originalCall: JsonRpcClientSingleMessage,
     public override val message: String,
 ) : RuntimeException("Fail to parse JsonRpc param: $message in: $originalCall")
+
+/**
+ * This exception can be thrown when the client or server want to response with [JsonRpcFailResponse].
+ * The framework will catch this exception and send a [JsonRpcFailResponse] to where the request comes from.
+ * Then the sender will receive a [JsonRpcFailResponse] and throw this exception again.
+ *
+ * @param error The [JsonRpcFailResponse.Error] in the [JsonRpcFailResponse].
+ */
+public class McpErrorResponseException(
+    public val error: JsonRpcFailResponse.Error,
+): RuntimeException("Get response with error:(${error.code}) ${error.message}")
